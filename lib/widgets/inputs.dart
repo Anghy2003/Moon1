2dfe81bb78cc4d52bae615e0b7075f259eeaef3d
Andrea <mercedes.illescas.est@tecazuay.edu.ec>
2025-05-20 17:23:20 -0500

    comit home

--- lib/widgets/inputs.dart
+++ lib/widgets/inputs.dart
@@ -7,6 +7,8 @@ class Inputs_Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    
     return TextFormField(
       decoration: InputDecoration(
         labelText: 'Usuario',
