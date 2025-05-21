5fd63c2ac4c23768b8b128c587945107c74e6d5f
Andrea <mercedes.illescas.est@tecazuay.edu.ec>
2025-05-21 00:24:22 -0500

    hotelmodificado

--- lib/Andrea/screens/hotel-detalles_screen.dart
+++ lib/Andrea/screens/hotel-detalles_screen.dart
@@ -1,94 +1,151 @@
 import 'package:flutter/material.dart';
- class HoteldetallesScreen extends  StatelessWidget {
- final String hotelName;
-  final String description;
-  final String imageUrl;
-  final List<String> facilities;
- const HoteldetallesScreen({
-    super.key,
-    this.hotelName = 'Nombre del hotel',
-    this.description = 'Descripción de ejemplo del hotel.',
-    this.imageUrl = 'https://a.travel-assets.com/findyours-php/viewfinder/images/res40/331000/331911-Grand-Fiesta-Americana-Coral-Beach-Cancun.jpg',
-    this.facilities = const ['WiFi', 'Piscina', 'Gimnasio'],
-  });
+
+
+class HoteldetallesScreen extends StatelessWidget {
+  const HoteldetallesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final facilities = [
+      {'icon': Icons.wifi, 'label': '1 Heater'},
+      {'icon': Icons.restaurant, 'label': 'Dinner'},
+      {'icon': Icons.bathtub, 'label': '1 Tub'},
+      {'icon': Icons.pool, 'label': 'Pool'},
+    ];
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(hotelName),
-        centerTitle: true,
-        backgroundColor: Colors.cyan[600],
-      ),
+      backgroundColor: const Color(0xFFebebf5),
       body: Column(
         children: [
-          // Imagen del hotel
-          Image.network(
-            imageUrl,
-            height: 220,
-            width: double.infinity,
-            fit: BoxFit.cover,
-          ),
+          const SizedBox(height: 30), // o más alto según cuánto quieras bajar la foto
 
-          // Descripcion y facilidades
+          Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(30),
+                  child: Image.network(
+                    'https://a.travel-assets.com/findyours-php/viewfinder/images/res40/331000/331911-Grand-Fiesta-Americana-Coral-Beach-Cancun.jpg',
+                    height: 280,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 20,
+                left: 30,
+                right: 30,
+                child: SizedBox(
+                  height: 60,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: List.generate(6, (index) {
+                      return Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 6),
+                        width: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                              'https://picsum.photos/id/10$index/200/200',
+                            ),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      );
+                    }),
+                  ),
+                ),
+              )
+            ],
+          ),
           Expanded(
             child: Padding(
-              padding: const EdgeInsets.all(16.0),
+              padding: const EdgeInsets.symmetric(horizontal: 20),
               child: ListView(
                 children: [
-                  Text(
-                    hotelName,
-                    style: const TextStyle(
+                  const Text(
+                    "Punta Cana",
+                    style: TextStyle(
                       fontSize: 24,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  const SizedBox(height: 12),
-                  Text(
-                    description,
-                    style: const TextStyle(fontSize: 16),
+                  const Row(
+                    children: [
+                      Icon(Icons.star, color: Colors.amber, size: 20),
+                      SizedBox(width: 4),
+                      Text("4.5 (355 Reviews)", style: TextStyle(fontSize: 14)),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  const Text(
+                    "Estadía para 3 personas en el lujoso Paradisus Palma Real, con comidas incluidas. "
+                    "Disfruta tours a Isla Saona y Altos de Chavón, con transporte incluido.",
+                    style: TextStyle(fontSize: 15),
                   ),
                   const SizedBox(height: 24),
                   const Text(
                     "Facilidades",
                     style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
-                  const SizedBox(height: 8),
-                  Wrap(
-                    spacing: 12,
-                    children: facilities.map((facility) {
-                      return Chip(
-                        label: Text(facility),
-                        backgroundColor: Colors.cyan[100],
-                      );
-                    }).toList(),
+                  const SizedBox(height: 12),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: facilities.map((facility) {
+                        return Container(
+                          width: 80,
+                          margin: const EdgeInsets.only(right: 12),
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: Colors.white.withOpacity(0.8),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Column(
+                            children: [
+                              Icon(facility['icon'] as IconData?, color: Colors.grey[500]),
+                              const SizedBox(height: 8),
+                              Text(
+                               facility['label'] as String,
+                                style: TextStyle(fontSize: 13, color: Colors.grey[600]),
+                                textAlign: TextAlign.center,
+                              ),
+                            ],
+                          ),
+                        );
+                      }).toList(),
+                    ),
                   ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    "Total",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  const Text(
+                    "\$799",
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.green),
+                  ),
+                  const SizedBox(height: 20),
+                  Center(
+                    child: IconButton(
+                      icon: const Icon(Icons.arrow_back, size: 30, color: Colors.cyan),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                 
                 ],
-              ),
-            ),
-          ),
-
-          // boton para continuar
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/select_dates');
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.cyan[600],
-                minimumSize: const Size.fromHeight(50),
-              ),
-              child: const Text(
-                'Reservar ahora',
-                style: TextStyle(fontSize: 18),
               ),
             ),
           ),
         ],
       ),
+      
     );
   }
 }
-
- 
