317baff6bf41161b128855717664eecf5d33fd89
Andrea <mercedes.illescas.est@tecazuay.edu.ec>
2025-05-21 19:11:01 -0500

    andrescorrecion

---
+++ lib/Andres/widgets/hotel_seleccionado.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:moon_aplication/Andrea/screens/hotel-detalles_screen.dart';
+import 'package:moon_aplication/Andres/widgets/menu_hoteles/tarjeta_hotel.dart';
+
+
+class HotelSeleccionado extends StatelessWidget {
+  final Map<String, dynamic> hotel;
+
+  const HotelSeleccionado({super.key, required this.hotel});
+
+  @override
+  Widget build(BuildContext context) {
+    return TarjetaHotel(
+      hotel: hotel,
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HoteldetallesScreen(), 
+          ),
+        );
+      },
+    );
+  }
+}
